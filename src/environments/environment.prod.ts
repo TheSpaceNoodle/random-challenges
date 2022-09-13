7e3a91d382a417c7b3210f15761249ec104ccb45
Volodymyr Shcherbyna <aknojlogiya@gmail.com>
2022-09-13 16:46:41 +0300

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'random-challenges-41e46',
+    appId: '1:941591829265:web:f20f39e38034e4330f4ab0',
+    storageBucket: 'random-challenges-41e46.appspot.com',
+    apiKey: 'AIzaSyCWcUs6t4fcD8F3jAnhq9gHIjCUTy-5A58',
+    authDomain: 'random-challenges-41e46.firebaseapp.com',
+    messagingSenderId: '941591829265',
+    measurementId: 'G-WKY5MQMH8B',
+  },
   production: true
 };
