02e16caf07ea5616eb4ea154e4e16f15418bee59
Volodymyr Shcherbyna <aknojlogiya@gmail.com>
2022-09-17 01:04:59 +0300

    hello-page template is done

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   scrolled = 0;
+  prevScrolled = 0;
+
+  @HostListener('window:scroll', ['$event.path[1].scrollY'])
+  handleScroll(scrollY: any) {
+    if (scrollY > this.prevScrolled && scrollY > 80) {
+      this.scrolled = 1;
+    } else if (this.prevScrolled > scrollY && scrollY != 0) {
+      this.scrolled = 2;
+    } else {
+      this.scrolled = 0;
+    }
+    this.prevScrolled = scrollY || 0;
+  }
 
   constructor() {}
 
